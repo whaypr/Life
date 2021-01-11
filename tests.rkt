efaae948be45441a660f01432b85e68815105be1
Patrik Drbal <drbalpat@fit.cvut.cz>
2021-01-11 14:43:46 +0100

    Add tests
    Some next-gen func unit tests

---
+++ tests.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+(require test-engine/racket-tests
+         "gol.rkt"
+         "demos.rkt")
+
+;  ----------  +-----------+  ----------  ;
+;  ----------  |   TESTS   |  ----------  ;
+;  ----------  +-----------+  ----------  ;
+
+(check-expect (next-gen '()) '())
+(check-expect (next-gen d-block 10) d-block)
+(check-expect (list->set (next-gen d-blinker 2))
+              (list->set d-blinker))
+(check-expect (list->set (next-gen d-glider 15))
+              (list->set '((5 5) (6 4) (6 5) (4 4) (5 6))))
+
+(test)
